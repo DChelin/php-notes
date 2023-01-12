48208c01462e4a062ee43e57117cf049bc5a911b
DChelin <55988126+DChelin@users.noreply.github.com>
2023-01-12 21:55:46 +0200

    PHP Learning Notes
    PHP Learning Notes - 12 Jan 2023

---
+++ 02_variables.php
@@ -0,0 +1,32 @@
+<?php
+/*---------- PHP Data Types ----------*?/
+/*
+- String            Series of characters surrounded by quotes
+- Interger          Whole numbers
+- Float             Decimal numbers
+- Boolean           true or false
+- Array             Special variable, which can hold more that onw value
+- Object            A class
+- NULL              Empty variable
+- Resource          Special variable that holds a resource
+*/
+
+/* -------- Variable Rules -------- */
+/*
+- Variables must be prefixed with $
+- Variables must start with a letter or the underscore character
+- Variables cant start with a number
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9 and _)
+- Variables are case-sensitive ($name and $NAME are two different variables)
+*/
+
+$name = "Brad"; //String
+$age = 40; //Interger
+$hasKids = true; //Boolean
+$cash_on_hand = 20.75; //Float
+
+//var_dump($has_kids);
+
+echo '$name is $age years old';
+
+//Stopped at 31:09
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "PHP Project 2023";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UACompatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'testing'; ?></h1>
+</body>
+</html>
+
